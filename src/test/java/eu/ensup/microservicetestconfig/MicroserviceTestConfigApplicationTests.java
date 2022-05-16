394d4a297cb8a5aba36b6ee36170d4c2808d903b
asemin08 <semin.allan@gmail.com>
2022-05-16 17:21:37 +0200

    first commit

---
+++ src/test/java/eu/ensup/microservicetestconfig/MicroserviceTestConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.ensup.microservicetestconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTestConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
